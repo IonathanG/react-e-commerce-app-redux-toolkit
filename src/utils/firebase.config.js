f404cab6e33062878dcc83e59cc8f4f74ca82f83
IonathanG <ionathangusmini@outlook.com>
2022-05-05 13:04:15 +0800

    first online push

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -12,10 +12,3 @@ const app = firebase.initializeApp({
 
 export const auth = app.auth();
 export default app;
-
-// apiKey: "AIzaSyCLXoc6r8kz3UaQzg1G8kWpZtTgi7JNZgU",
-// authDomain: "react-ecommerce-b3df9.firebaseapp.com",
-// projectId: "react-ecommerce-b3df9",
-// storageBucket: "react-ecommerce-b3df9.appspot.com",
-// messagingSenderId: "242947292358",
-// appId: "1:242947292358:web:a17fc8c99c3ef012f0abc7",
