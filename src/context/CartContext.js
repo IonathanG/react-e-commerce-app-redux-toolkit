2a45a93012fa5bd18a098328978282e565740a16
IonathanG <ionathangusmini@outlook.com>
2022-05-08 16:01:25 +0800

    local storage added

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,10 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [listItems, setListItems] = useState([]);
   const [totalQuantity, setTotalQuantity] = useState(0);
+
+  //check local storage for items on page reload
+  useEffect(() => {
+    let storedData = window.localStorage.listItems
+      ? JSON.parse(localStorage.getItem("listItems"))
+      : [];
+    setListItems(storedData);
+  }, []);
+
+  //update local storage with item list on change
+  useEffect(() => {
+    localStorage.setItem("listItems", JSON.stringify(listItems));
+  }, [listItems, totalQuantity]);
+
+  //update total quantity on reload
+  useEffect(() => {
+    setTotalQuantity(0);
+    for (let i = 0; i < listItems.length; i++) {
+      setTotalQuantity((prevState) => prevState + listItems[i].quantity);
+    }
+  }, [listItems]);
 
   //add item to the cart or add quantity of an item already in the cart
   const addItem = (name, quantity, price) => {
@@ -37,6 +58,7 @@ export function CartProvider({ children }) {
     }
   };
 
+  //adding quantity to an item from the cart
   const addQuantity = (name) => {
     for (let i = 0; i < listItems.length; i++) {
       if (listItems[i].name === name) {
@@ -46,6 +68,7 @@ export function CartProvider({ children }) {
     }
   };
 
+  //removing quantity to an item from the cart
   const removeQuantity = (name) => {
     for (let i = 0; i < listItems.length; i++) {
       if (listItems[i].name === name) {
